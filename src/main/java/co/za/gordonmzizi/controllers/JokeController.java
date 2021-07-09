be12a1782abac57704699288f7721520b80a3330
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-07-10 00:36:26 +0200

    Creating controller. close #4

---
+++ src/main/java/co/za/gordonmzizi/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package co.za.gordonmzizi.controllers;
+
+import co.za.gordonmzizi.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    private String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
